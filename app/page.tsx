2dfdc1f86b6260bc5215f1477bf976d20b90e8dd
Divya-Darshan <parukrish.darshan20@gmail.com>
2026-02-08 21:40:45 +0530

    imgkit init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+
 import Profile from '@/components/profile/profile'
+import { ImageKitProvider, Image } from "@imagekit/next";
+
+
+
 export default function Home() {
   return (
           <>
-          
+
+         
+            <ImageKitProvider  urlEndpoint="https://ik.imagekit.io/weeb">
+            
+            <Image  src="a"      
+             width={500}
+             height={500}
+             loading="lazy" 
+             alt="Picture of the author" />
+             
+            </ImageKitProvider>
+                      
           <Profile/>
 
           
