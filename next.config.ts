dce2f116139ab1c6f3022a022a9729b67e9fa4ab
Divya Darshan <parukrish.darshan20@gmail.com>
2026-04-27 09:39:49 +0100

    logo change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['172.19.73.150', 'local-origin.dev', '*.local-origin.dev'],
+  allowedDevOrigins: ['10.153.211.150', 'local-origin.dev', '*.local-origin.dev'],
 }
 
 export default nextConfig
